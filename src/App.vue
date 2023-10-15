4d4d63d3479d0b2e1a255e6c02778fd17aac53e4
SabaMuladze <muladze66@bk.ru>
2023-10-15 23:22:14 +0400

    style:add desktop and tablet versions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 
 <template>
-  <div class="w-full min-h-screen flex flex-col bg-white  md:w-fit">
-    <div class="img h-[285px] bg-cover bg-no-repeat"></div>
-    <div class="px-6">
+  <div
+    class="w-full h-screen flex flex-col bg-white  md:w-fit lg:flex-row-reverse md:rounded-lg lg:rounded-[36px] lg:w-[930px] lg:h-[630px] lg:pr-5">
+    <div class="img h-[285px] bg-cover bg-no-repeat md:rounded-lg lg:w-[50%]"></div>
+    <div class="px-6 lg:px-16 md:py-5 lg:py-24">
       <div class="flex flex-col gap-6">
-        <h1 class="mt-6 text-4xl">Stay updated!</h1>
+        <h1 class="mt-6 text-4xl md:text-[56px]">Stay updated!</h1>
         <p class="mb-6">Join 60,000+ product managers receiving monthly updates on:</p>
       </div>
 
@@ -20,20 +21,21 @@
           width="21">
         <p>And much more!</p>
       </div>
-    </div>
-    <div class="px-6 mt-10">
-      <label for="email" class="font-bold">Email Adress</label>
-      <div class=" border-solid border-[1px] border-[rgba(25, 24, 43, 0.25)] rounded-lg"><input
-          class="px-6 py-4 w-full rounded-lg h-[]" name="email" type="email">
-      </div>
+      <div class="mt-10">
+        <label for="email" class="font-bold">Email Adress</label>
+        <div class=" border-solid border-[1px] border-[rgba(25, 24, 43, 0.25)] rounded-lg"><input
+            class="px-6 py-4 w-full rounded-lg outline-none" name="email" type="email" placeholder="email@company.com">
+        </div>
 
-      <button @click="send" class="bg-[#242742] text-white font-bold w-full py-5 px-8 rounded-lg mt-6">Subscribe to
-        monthly
-        newsletter</button>
+        <button @click="send" class="bg-[#242742] text-white font-bold w-full py-5 px-8 rounded-lg mt-6">Subscribe to
+          monthly
+          newsletter</button>
+      </div>
     </div>
+
   </div>
   <div v-if="showSection"
-    class="w-full min-h-screen bg-white absolute top-0 flex flex-col justify-between px-6 pb-10 md:w-fit">
+    class="w-full min-h-screen bg-white absolute top-0 flex flex-col justify-between px-6 pb-6 md:w-fit">
     <div class="mt-[150px]">
       <img src="./assets/images/icon-success.svg" height="64" width="64" alt="">
       <h1 class="mt-10 text-4xl mb-6">Thanks for subscribing!</h1>
@@ -95,8 +97,12 @@ h1 {
 }
 
 @media (min-width:768px) {
-  img {
+  .img {
     background: url(./assets/images/illustration-sign-up-desktop.svg);
+    background-size: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100%;
   }
 }
 </style>
